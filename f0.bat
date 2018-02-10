16345ef28cb48a2f83648ea29ad5120e1541d66e
wmThompson1 <wm.thompson1@gmail.com>
2018-02-10 13:39:50 -0800

    Friday synch

--- f0.bat
+++ f0.bat
@@ -1,8 +1,8 @@
 
 git add -A
 git commit -m "Friday synch"
-## git push -u origin master
-## git push https://github.com/wmthompson1/seed3.git 
-## to force use -f
+:: git push -u origin master
+:: git push https://github.com/wmthompson1/seed3.git 
+:: to force use -f
 git push -f origin master
 PAUSE
